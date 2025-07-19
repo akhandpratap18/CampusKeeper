f48bcf0867d0cf0e60fd31062626aab2d0dd5b80
Akhand Pratap Singh <akhandpratapsingh1817@gmail.com>
2025-07-19 21:41:32 +0530

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -6,13 +6,13 @@ import { getStorage } from "firebase/storage";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA0Sl4ApJpUs8YfyQyCcz_oV4DK4THf-Ns",
-  authDomain: "campuskeeper-d7531.firebaseapp.com",
-  projectId: "campuskeeper-d7531",
-  storageBucket: "campuskeeper-d7531.appspot.com",
-  messagingSenderId: "983941932831",
-  appId: "1:983941932831:web:a0de20de08938d9f4d722a",
-  measurementId: "G-Z5TDLEWZ7P",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 
